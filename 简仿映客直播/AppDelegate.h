0ca532ee62f1f583edbfb43fac85ab6f601571a7
Allen <495544258@qq.com>
2016-09-01 17:58:09 +0800

    “test”

---
+++ 简仿映客直播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  简仿映客直播
+//
+//  Created by apple on 16/6/22.
+//  Copyright © 2016年 下载链接 http://pan.baidu.com/s/1kUR5GcJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 简仿映客直播/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  简仿映客直播
+//
+//  Created by apple on 16/6/22.
+//  Copyright © 2016年 下载链接 http://pan.baidu.com/s/1kUR5GcJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
